670b94445e36f63765828b8df9cdbca3a022d25b
Woodney Juste <woodneydev@gmail.com>
2023-08-10 17:20:32 -0400

    post added

--- routes/videos.js
+++ routes/videos.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const fs = require("fs");
+const crypto = require("crypto");
 
 const readVideoDetails = () => {
     let videos = fs.readFileSync("./data/videos.json");
@@ -22,6 +23,10 @@ const getVideoDetail = (id) => {
     return result;
 }
 
+const createId = (length) => {
+    return crypto.randomBytes(length).toString("hex");
+}
+
 //Routes
 router.get("/", (req, res) => {
     const videos = getVideos();
@@ -32,6 +37,29 @@ router.get("/:id", (req, res) => {
     const {id} = req.params
     const videoDetail = getVideoDetail(id);
     res.status(200).json(videoDetail);
+})
+
+router.post("/", (req, res) => {
+    const videoDetails = readVideoDetails();
+    const {data} = req.body
+
+    const postVideoObj = {
+        ...data, 
+        id: `${createId(4)}-${createId(2)}-${createId(2)}-${createId(6)}`, 
+        channel: "n/a",
+        image: " ",
+        views: " ",
+        likes: " ",
+        duration: " ",
+        video: " ",
+        timestamp: " ",
+        comments: []
+    }
+
+    videoDetails.push(postVideoObj)
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videoDetails));
+    res.status(201).json(postVideoObj);
+
 })
 
 module.exports = router;
